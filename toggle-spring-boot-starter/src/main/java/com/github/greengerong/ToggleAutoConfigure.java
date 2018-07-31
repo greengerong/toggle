d4323223c991d8e3d594747e73a677cce510f79a
polang <grzx2210@163.com>
2018-07-31 18:07:30 +0800

    [greengerong] feature toggle spring starter

--- toggle-spring-boot-starter/src/main/java/com/github/greengerong/ToggleAutoConfigure.java
+++ toggle-spring-boot-starter/src/main/java/com/github/greengerong/ToggleAutoConfigure.java
@@ -1,8 +1,10 @@
 package com.github.greengerong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,33 +24,28 @@ import org.springframework.context.annotation.Configuration;
 public class ToggleAutoConfigure {
 
     private final ToggleConfig toggleConfig;
-    private ToggleService toggleService;
 
     @Autowired
     public ToggleAutoConfigure(ToggleConfig toggleConfig) {
         this.toggleConfig = toggleConfig;
-        toggleService = new ToggleService(toggleConfig);
     }
 
     @Bean
     @ConditionalOnMissingBean
-//    @Order(100)
     public ToggleService toggleService() {
-        return toggleService;
+        return new ToggleService(toggleConfig);
     }
 
     @Bean
-//    @ConditionalOnMissingBean
-//    @ConditionalOnProperty(prefix = "feature-toggle", value = "store-way", havingValue = "properties", matchIfMissing = true)
-//    @Order(10)
-    public FeatureToggleAspect featureToggleAspect() {
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "feature-toggle", value = "store-way", havingValue = "properties", matchIfMissing = true)
+    public FeatureToggleAspect featureToggleAspect(@Autowired ToggleService toggleService) {
         return new FeatureToggleAspect(toggleService);
     }
 
-    //    @ConditionalOnBean(ToggleService.class)
-//    @Order(10)
     @Bean
-    public ToggleEndpoint toggleEndpoint() {
+    @ConditionalOnBean(ToggleService.class)
+    public ToggleEndpoint toggleEndpoint(@Autowired ToggleService toggleService) {
         return new ToggleEndpoint(toggleService);
     }
 }
