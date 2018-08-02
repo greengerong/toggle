3c0a648a0818823b9e70fe8b07321918ec39a3e5
polang <grzx2210@163.com>
2018-08-02 10:39:27 +0800

    [greengerong] samples

--- samples/toggle-spring-jdbc-sample/src/main/java/com/github/greengerong/Application.java
+++ samples/toggle-spring-jdbc-sample/src/main/java/com/github/greengerong/Application.java
@@ -2,9 +2,8 @@ package com.github.greengerong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
---
+++ samples/toggle-spring-jdbc-sample/src/main/java/com/github/greengerong/DemoController.java
@@ -0,0 +1,32 @@
+package com.github.greengerong;
+
+import com.github.greengerong.aspect.FeatureToggle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/******************************************
+ *                                        *
+ * Auth: green gerong                     *
+ * Date: 2018                             *
+ * blog: http://greengerong.github.io/    *
+ * github: https://github.com/greengerong *
+ *                                        *
+ ******************************************/
+@RestController()
+@RequestMapping("demo")
+public class DemoController {
+
+    @FeatureToggle("foo")
+    @GetMapping("foo")
+    public String foo() {
+        return "foo call";
+    }
+
+    @FeatureToggle("bar")
+    @GetMapping("bar")
+    public String bar() {
+        return "bar call";
+    }
+}
